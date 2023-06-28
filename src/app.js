5ce446ed8f57fb40a47d8000f95935d30f04913d
Sebastian Gaggero Díaz <49600552+SrMetus@users.noreply.github.com>
2023-06-28 02:43:01 +0000

    Puras Excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let mensaje = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomExcuse = arreglo => {
+    let index = Math.floor(Math.random() * arreglo.length);
+    return arreglo[index];
+  };
   console.log("Hello Rigo from the console!");
+  let excusa = `${randomExcuse(who)}, ${randomExcuse(action)}, ${randomExcuse(
+    what
+  )}, ${randomExcuse(when)}`;
+  mensaje.textContent = excusa;
 };
